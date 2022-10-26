3960956e5d8c28861e3d43e7c06d3425a13ca760
malyna2 <57286096+malyna2@users.noreply.github.com>
2022-10-26 21:57:13 +0300

    added basic routes modified swagger, models filling database

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@ from waitress import serve
 from routes import app
 
 
-
 if __name__ == '__main__':
     app.run(debug=True)
     serve(app)
--- backend/routes.py
+++ backend/routes.py
@@ -1,27 +1,61 @@
 from flask import request, Flask
+from models import *
 import json
 
 app = Flask(__name__)
 
 
-class User:
-    def __init__(self, name, id):
-        self.name = name
-        self.id = id
+@app.route('/team/findByCompany', methods=['GET'])
+def find_team_by_company():
+	args = request.args
+	company_id = args.get('company_id')
+	teams = Session.query(Team).filter(Team.company_id == company_id)
+	return json.dumps([i.to_dict() for i in teams])
 
 
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=True, indent=2)
+@app.route('/employee/findByCompany', methods=['GET'])
+def find_employee_by_company():
+	args = request.args
+	company_id = args.get('company_id')
+	employees = Session.query(Employee).filter(Employee.company_id == company_id)
+	return json.dumps([i.to_dict() for i in employees])
 
 
-@app.route('/propertySet', methods=['POST', 'GET'])
-def hello_world():
-    request_data = request.get_json()
-    user = User(name=request_data['name'], id=1)
-    return user.toJSON()
+@app.route('/employee/findByTeam', methods=['GET'])
+def find_employee_by_team():
+	args = request.args
+	team_id = args.get('team_id')
+	employees = Session.query(Employee).filter(Employee.team_id == team_id)
+	return json.dumps([i.to_dict() for i in employees])
 
 
-@app.route('/api/v1/show')
-def show_number():
-    return "4", 200
+@app.route('/answer/', methods=['GET'])
+def find_answer_by_feedback_id():
+	args = request.args
+	feedback_id = args.get('feedback_id')
+	answers = Session.query(Answer).filter(Answer.feedback_id == feedback_id)
+	return json.dumps([i.to_dict() for i in answers])
+
+
+@app.route('/feedback/', methods=['GET'])
+def find_feedback_by_employee_id():
+	args = request.args
+	employee_id = args.get('employee_id')
+	feedbacks = Session.query(Feedback).filter(Feedback.employee_id == employee_id)
+	return json.dumps([i.to_dict() for i in feedbacks])
+
+
+@app.route('/question/', methods=['GET'])
+def find_question_by_property_set_id():
+	args = request.args
+	property_set_id = args.get('property_set_id')
+	questions = Session.query(Question).filter(Question.property_set_id == property_set_id)
+	return json.dumps([i.to_dict() for i in questions])
+
+
+@app.route('/property_set/', methods=['GET'])
+def find_property_set():
+	args = request.args
+	property_set_id = args.get('property_set_id')
+	property_sets = Session.query(PropertySet).filter(PropertySet.id == property_set_id)
+	return json.dumps([i.to_dict() for i in property_sets])
