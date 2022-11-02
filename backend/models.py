2c9868acec07213036a49c8ac4de1c9b992afaa7
malyna2 <57286096+malyna2@users.noreply.github.com>
2022-11-02 19:55:41 +0200

    Employee, company

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,5 @@
-from uuid import UUID
-
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base, scoped_session
-from sqlalchemy_serializer import SerializerMixin
 from marshmallow import Schema, fields, validate, ValidationError
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
@@ -14,140 +11,144 @@ Base.metadata.create_all(bind=engine)
 session = Session()
 
 
-class CustomSerializerMixin(SerializerMixin):
-    serialize_types = (
-        (UUID, lambda x: str(x)),
-    )
+class Company(Base):
+	__tablename__ = 'company'
+
+	id = Column('id', Integer, primary_key=True, autoincrement=True)
+	name = Column('name', String(45))
 
 
-class Company(Base, CustomSerializerMixin):
-    __tablename__ = 'company'
-
-    serialize_only = {'id', 'name'}
-
-    id = Column('id', Integer, primary_key=True, autoincrement=True)
-    name = Column('name', String(45))
+class CompanySchema(SQLAlchemyAutoSchema):
+	class Meta:
+		model = Company
+		include_relationships = False
+		load_instance = True
+		include_fk = True
 
 
 class Team(Base):
-    __tablename__ = 'team'
+	__tablename__ = 'team'
 
-    id = Column('id', Integer, primary_key=True, autoincrement=True)
-    tag = Column('tag', String(45))
-    name = Column('name', String(45))
-    company_id = Column('company_id', Integer, ForeignKey(Company.id))
-    company = relationship(Company, backref='team', lazy='joined')
+	id = Column('id', Integer, primary_key=True, autoincrement=True)
+	tag = Column('tag', String(45))
+	name = Column('name', String(45))
+	company_id = Column('company_id', Integer, ForeignKey(Company.id))
+	company = relationship(Company, backref='team', lazy='joined')
 
 
 class TeamSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Team
-        include_relationships = True
-        load_instance = True
-        include_fk = True
+	class Meta:
+		model = Team
+		include_relationships = True
+		load_instance = True
+		include_fk = True
 
 
-class Employee(Base, CustomSerializerMixin):
-    __tablename__ = 'employee'
+class Employee(Base):
+	__tablename__ = 'employee'
 
-    id = Column('id', Integer, primary_key=True, autoincrement=True)
-    first_name = Column('first_name', String(15), nullable=False)
-    last_name = Column('last_name', String(15), nullable=False)
-    email = Column('email', String(30), nullable=False)
+	id = Column('id', Integer, primary_key=True, autoincrement=True)
+	first_name = Column('first_name', String(15), nullable=False)
+	last_name = Column('last_name', String(15), nullable=False)
+	email = Column('email', String(30), nullable=False)
 
-    company_id = Column('company_id', Integer, ForeignKey(Company.id))
-    company = relationship(Company, backref='employee', lazy='joined')
+	company_id = Column('company_id', Integer, ForeignKey(Company.id))
+	company = relationship(Company, backref='employee', lazy='joined')
 
-    team_id = Column('team_id', Integer, ForeignKey(Team.id))
-    team = relationship(Team, backref='employee', lazy='joined')
+	team_id = Column('team_id', Integer, ForeignKey(Team.id))
+	team = relationship(Team, backref='employee', lazy='joined')
 
-    is_owner = Column('is_owner', Boolean, nullable=False)
-    password = Column('password', String(20), nullable=False)
-    phone = Column('phone', String(13), nullable=False)
-    date_of_birth = Column('date_of_birth', DATE, nullable=False)
-    role = Column('role', Boolean, nullable=False)
+	is_owner = Column('is_owner', Boolean, nullable=False)
+	password = Column('password', String(20), nullable=False)
+	phone = Column('phone', String(13), nullable=False)
+	date_of_birth = Column('date_of_birth', DATE, nullable=False)
+	role = Column('role', Boolean, nullable=False)
+
+
+class EmployeeSchema(SQLAlchemyAutoSchema):
+	class Meta:
+		model = Employee
+		include_relationships = False
+		load_instance = True
+		include_fk = True
 
 
 class PropertySet(Base):
-    __tablename__ = 'property_set'
+	__tablename__ = 'property_set'
 
-    id = Column('id', Integer, primary_key=True, autoincrement=True)
-    name = Column('name', String(45))
+	id = Column('id', Integer, primary_key=True, autoincrement=True)
+	name = Column('name', String(45))
 
 
 class PropertySetSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = PropertySet
-        include_relationships = True
-        load_instance = True
-        include_fk = True
+	class Meta:
+		model = PropertySet
+		include_relationships = True
+		load_instance = True
+		include_fk = True
 
 
-class Question(Base, CustomSerializerMixin):
-    __tablename__ = 'question'
+class Question(Base):
+	__tablename__ = 'question'
 
-    serialize_only = {'id', 'number', 'text', 'property_set_id'}
-
-    id = Column('id', Integer, primary_key=True, autoincrement=True)
-    number = Column('number', Integer, nullable=False)
-    text = Column('text', String(100), nullable=False)
-    property_set_id = Column('property_set_id', Integer, ForeignKey(PropertySet.id))
-    property_set = relationship(PropertySet, backref='question', lazy='joined')
+	id = Column('id', Integer, primary_key=True, autoincrement=True)
+	number = Column('number', Integer, nullable=False)
+	text = Column('text', String(100), nullable=False)
+	property_set_id = Column('property_set_id', Integer, ForeignKey(PropertySet.id))
+	property_set = relationship(PropertySet, backref='question', lazy='joined')
 
 
 class QuestionSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Question
-        include_relationships = True
-        load_instance = True
-        include_fk = True
+	class Meta:
+		model = Question
+		include_relationships = True
+		load_instance = True
+		include_fk = True
 
 
-class FeedbackHistory(Base, CustomSerializerMixin):
-    __tablename__ = 'feedback_history'
+class FeedbackHistory(Base):
+	__tablename__ = 'feedback_history'
 
-    serialize_only = {'employee_id', 'feedback_history', 'property_set_id'}
-
-    employee_id = Column('employee_id', Integer, ForeignKey(Employee.id), primary_key=True)
-    employee = relationship(Employee, backref='feedback_history', lazy='joined')
-    property_set_id = Column('property_set_id', Integer, ForeignKey(PropertySet.id))
-    property_set = relationship(PropertySet, backref='feedback_history', lazy='joined')
+	employee_id = Column('employee_id', Integer, ForeignKey(Employee.id), primary_key=True)
+	employee = relationship(Employee, backref='feedback_history', lazy='joined')
+	property_set_id = Column('property_set_id', Integer, ForeignKey(PropertySet.id))
+	property_set = relationship(PropertySet, backref='feedback_history', lazy='joined')
 
 
 class Feedback(Base):
-    __tablename__ = 'feedback'
+	__tablename__ = 'feedback'
 
-    id = Column('id', Integer, primary_key=True, autoincrement=True)
-    date_of_creation = Column('date_of_creation', DATE, nullable=False)
-    note = Column('note', String(500), nullable=False)
-    employee_id = Column('employee_id', Integer, ForeignKey(FeedbackHistory.employee_id))
-    feedbackHistory = relationship(FeedbackHistory, backref='feedback', lazy='joined')
+	id = Column('id', Integer, primary_key=True, autoincrement=True)
+	date_of_creation = Column('date_of_creation', DATE, nullable=False)
+	note = Column('note', String(500), nullable=False)
+	employee_id = Column('employee_id', Integer, ForeignKey(FeedbackHistory.employee_id))
+	feedbackHistory = relationship(FeedbackHistory, backref='feedback', lazy='joined')
 
 
 class FeedbackSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Feedback
-        include_relationships = True
-        load_instance = True
-        include_fk = True
+	class Meta:
+		model = Feedback
+		include_relationships = True
+		load_instance = True
+		include_fk = True
 
 
 class Answer(Base):
-    __tablename__ = 'answer'
+	__tablename__ = 'answer'
 
-    id = Column('id', Integer, primary_key=True, autoincrement=True)
-    number = Column('number', Integer, nullable=False)
-    text = Column('text', String(100), nullable=False)
-    feedback_id = Column('feedback_id', Integer, ForeignKey(Feedback.id))
-    feedback = relationship(Feedback, backref='answer', lazy='joined')
+	id = Column('id', Integer, primary_key=True, autoincrement=True)
+	number = Column('number', Integer, nullable=False)
+	text = Column('text', String(100), nullable=False)
+	feedback_id = Column('feedback_id', Integer, ForeignKey(Feedback.id))
+	feedback = relationship(Feedback, backref='answer', lazy='joined')
 
 
 class AnswerSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Answer
-        include_relationships = True
-        load_instance = True
-        include_fk = True
+	class Meta:
+		model = Answer
+		include_relationships = True
+		load_instance = True
+		include_fk = True
 
 
 Base.metadata.create_all(bind=engine)
--- backend/routes.py
+++ backend/routes.py
@@ -5,50 +5,44 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/team/findByCompany', methods=['GET'])
-def find_team_by_company():
-	args = request.args
-	company_id = args.get('company_id')
-	teams = Session.query(Team).filter(Team.company_id == company_id)
-	return json.dumps([i.to_dict() for i in teams])
-
-
-@app.route('/employee/findByCompany', methods=['GET'])
-def find_employee_by_company():
-	args = request.args
-	company_id = args.get('company_id')
-	employees = Session.query(Employee).filter(Employee.company_id == company_id)
-	return json.dumps([i.to_dict() for i in employees])
-
-
-@app.route('/employee/findByTeam', methods=['GET'])
-def find_employee_by_team():
-	args = request.args
-	team_id = args.get('team_id')
-	employees = Session.query(Employee).filter(Employee.team_id == team_id)
-	return json.dumps([i.to_dict() for i in employees])
-
-
-@app.route('/answer', methods=['POST'])
-def new_answer():
+@app.route('/propertySet', methods=['POST'])
+def new_property_set():
 	args = request.get_json()
 	try:
-		answer_schema = AnswerSchema()
-		answer = answer_schema.load(args, session=session)
-		session.add(answer)
+		property_set_schema = PropertySetSchema()
+		property_set = property_set_schema.load(args, session=session)
+		session.add(property_set)
 		session.commit()
-		return answer_schema.dump(answer)
+		return property_set_schema.dump(property_set)
 	except ValidationError as err:
 		return str(err)
 
 
-@app.route('/answer/', methods=['GET'])
-def find_answer_by_feedback_id():
+@app.route('/propertySet/', methods=['GET'])
+def find_property_set():
 	args = request.args
-	feedback_id = args.get('feedback_id')
-	answers = session.query(Answer).filter(Answer.feedback_id == feedback_id)
-	answer_schema = AnswerSchema()
-	return json.dumps([answer_schema.dump(i) for i in answers])
+	property_set_id = args.get('property_set_id')
+	property_set = session.query(PropertySet).filter(PropertySet.id == property_set_id).first()
+	property_set_schema = PropertySetSchema()
+	return property_set_schema.dump(property_set)
+
+# Needed delete feedback_history... redirect
+#@app.route('/propertySet/', methods=['DELETE'])
+def delete_property_set():
+	args = request.args
+	property_set_id = args.get('property_set_id')
+	session.query(Question).filter(Question.property_set_id == property_set_id).delete()
+	session.query(FeedbackHistory).filter(FeedbackHistory.property_set_id == property_set_id).delete()
+	session.query(PropertySet).filter(PropertySet.id == property_set_id).delete()
+	session.commit()
+	return "Feedback deleted"
+
+
+@app.route('/propertySets/', methods=['GET'])
+def find_all_property_set():
+	property_sets = session.query(PropertySet)
+	property_set_schema = PropertySetSchema()
+	return json.dumps([property_set_schema.dump(i) for i in property_sets])
 
 
 @app.route('/feedback', methods=['POST'])
@@ -92,6 +86,106 @@ def find_feedback():
 	return feedback_schema.dump(feedback)
 
 
+@app.route('/employee/', methods=['POST'])
+def new_employee():
+	args = request.get_json()
+	try:
+		employee_schema = EmployeeSchema()
+		employee = employee_schema.load(args, session=session)
+		session.add(employee)
+		session.commit()
+		return employee_schema.dump(employee)
+	except ValidationError as err:
+		return str(err)
+
+
+@app.route('/employee/', methods=['GET'])
+def find_employee():
+	args = request.args
+	employee_id = args.get('employee_id')
+	employee = Session.query(Employee).filter(Employee.id == employee_id)
+	return json.dumps([i.to_dict() for i in employee])
+
+
+@app.route('/employee/findByCompany', methods=['GET'])
+def find_employee_by_company():
+	args = request.args
+	company_id = args.get('company_id')
+	employees = Session.query(Employee).filter(Employee.company_id == company_id)
+	return json.dumps([i.to_dict() for i in employees])
+
+
+@app.route('/employee/findByTeam', methods=['GET'])
+def find_employee_by_team():
+	args = request.args
+	team_id = args.get('team_id')
+	employees = Session.query(Employee).filter(Employee.team_id == team_id)
+	return json.dumps([i.to_dict() for i in employees])
+
+@app.route('/company/', methods=['POST'])
+def new_company():
+	args = request.get_json()
+	try:
+		company_schema = CompanySchema()
+		company = company_schema.load(args, session=session)
+		session.add(company)
+		session.commit()
+		return company_schema.dump(company)
+	except ValidationError as err:
+		return str(err)
+
+
+@app.route('/company/', methods=['GET'])
+def find_company():
+	args = request.args
+	company_id = args.get('company_id')
+	company = session.query(Company).filter(Company.id == company_id).first()
+	company_schema = CompanySchema()
+	return company_schema.dump(company)
+
+
+@app.route('/company/', methods=['DELETE'])
+def delete_company():
+	args = request.args
+	company_id = args.get('company_id')
+
+	"""redirect to employee, feedback history, feedback, answer needed"""
+
+	session.query(Company).filter(Company.id == company_id).delete()
+	session.commit()
+	return "Company deleted"
+
+
+@app.route('/team/findByCompany', methods=['GET'])
+def find_team_by_company():
+	args = request.args
+	company_id = args.get('company_id')
+	teams = Session.query(Team).filter(Team.company_id == company_id)
+	return json.dumps([i.to_dict() for i in teams])
+
+
+@app.route('/answer', methods=['POST'])
+def new_answer():
+	args = request.get_json()
+	try:
+		answer_schema = AnswerSchema()
+		answer = answer_schema.load(args, session=session)
+		session.add(answer)
+		session.commit()
+		return answer_schema.dump(answer)
+	except ValidationError as err:
+		return str(err)
+
+
+@app.route('/answer/', methods=['GET'])
+def find_answer_by_feedback_id():
+	args = request.args
+	feedback_id = args.get('feedback_id')
+	answers = session.query(Answer).filter(Answer.feedback_id == feedback_id)
+	answer_schema = AnswerSchema()
+	return json.dumps([answer_schema.dump(i) for i in answers])
+
+
 @app.route('/question', methods=['POST'])
 def new_question():
 	args = request.get_json()
@@ -112,53 +206,4 @@ def find_question_by_property_set_id():
 	questions = session.query(Question).filter(Question.property_set_id == property_set_id)
 	question_schema = AnswerSchema()
 	return json.dumps([question_schema.dump(i) for i in questions])
-
-
-@app.route('/propertySet', methods=['POST'])
-def new_property_set():
-	args = request.get_json()
-	try:
-		property_set_schema = PropertySetSchema()
-		property_set = property_set_schema.load(args, session=session)
-		session.add(property_set)
-		session.commit()
-		return property_set_schema.dump(property_set)
-	except ValidationError as err:
-		return str(err)
-
-
-@app.route('/propertySet/', methods=['GET'])
-def find_property_set():
-	args = request.args
-	property_set_id = args.get('property_set_id')
-	property_set = session.query(PropertySet).filter(PropertySet.id == property_set_id).first()
-	property_set_schema = PropertySetSchema()
-	return property_set_schema.dump(property_set)
-
-# Needed delete feedback_history... redirect
-#@app.route('/propertySet/', methods=['DELETE'])
-def delete_property_set():
-	args = request.args
-	property_set_id = args.get('property_set_id')
-	session.query(Question).filter(Question.property_set_id == property_set_id).delete()
-	session.query(FeedbackHistory).filter(FeedbackHistory.property_set_id == property_set_id).delete()
-	session.query(PropertySet).filter(PropertySet.id == property_set_id).delete()
-	session.commit()
-	return "Feedback deleted"
-
-
-@app.route('/propertySets/', methods=['GET'])
-def find_all_property_set():
-	property_sets = session.query(PropertySet)
-	property_set_schema = PropertySetSchema()
-	return json.dumps([property_set_schema.dump(i) for i in property_sets])
-
-
-@app.route('/employee/', methods=['GET'])
-def find_employee():
-	args = request.args
-	employee_id = args.get('employee_id')
-	employee = Session.query(Employee).filter(Employee.id == employee_id)
-	return json.dumps([i.to_dict() for i in employee])
-
 
